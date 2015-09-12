3d2619f541e73fc8687af0f6fc8b0f94b195a2cd
The-Quill <baileysheather.98@gmail.com>
2015-09-12 20:18:52 +1000

    added meta

--- design.user.js
+++ design.user.js
@@ -6,6 +6,8 @@
 // @author       Quill
 // @match        *//codereview.stackexchange.com
 // @match		 *//codereview.stackexchange.com/*
+// @match        *//meta.codereview.stackexchange.com
+// @match		 *//meta.codereview.stackexchange.com/*
 // @grant		 GM_addStyle
 // @grant        GM_getResourceText
 // @resource dark_theme https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/design.css
