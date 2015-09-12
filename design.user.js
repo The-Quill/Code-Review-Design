f9dbd47bf55460bc527feac36333c60364ffc4a2
The-Quill <baileysheather.98@gmail.com>
2015-09-12 20:06:53 +1000

    moved into new version

---
+++ design.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         CodeReview dark theme
+// @namespace    https://github.com/The-Quill/Code-Review-Design
+// @version      0.1
+// @description  A new Code Review theme.
+// @author       Quill
+// @match        *//codereview.stackexchange.com
+// @match		 *//codereview.stackexchange.com/*
+// @grant		 GM_addStyle
+// @grant        GM_getResourceText
+// @resource dark_theme https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/design.css
+// ==/UserScript==
+
+GM_addStyle(GM_getResourceText("dark_theme"));
