10a87a48d67af28602644d2efac4a87a25193e30
The-Quill <baileysheather.98@gmail.com>
2015-09-14 15:58:21 +1000

    moved css link structure, fixed the missing _skin

--- design.user.js
+++ design.user.js
@@ -1,24 +1,25 @@
 // ==UserScript==
-// @name	CodeReview dark theme
-// @version	0.7
+// @name		CodeReview dark theme
+// @namespace 	https://github.com/The-Quill/Code-Review-Design
+// @version		0.8
 // @description	A new Code Review theme.
-// @author	Quill
-// @match	*://codereview.stackexchange.com
-// @match	*://codereview.stackexchange.com/*
-// @match	*://meta.codereview.stackexchange.com
-// @match	*://meta.codereview.stackexchange.com/*
-// @grant	GM_addStyle
-// @grant	GM_getResourceText
-// @resource	theme https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/design.css
-// @resource	main_skin https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/main.css
-// @resource	meta_skin https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/meta.css
+// @author		Quill
+// @match		*://codereview.stackexchange.com
+// @match		*://codereview.stackexchange.com/*
+// @match		*://meta.codereview.stackexchange.com
+// @match		*://meta.codereview.stackexchange.com/*
+// @grant		GM_addStyle
+// @grant		GM_getResourceText
+// @resource	theme 	  https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/resources/design.css
+// @resource	main_skin https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/resources/main.css
+// @resource	meta_skin https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/resources/meta.css
 // ==/UserScript==
 
 GM_addStyle(
 	GM_getResourceText(
-		window.location.href.contains('://meta.codereview')
+		(window.location.href.contains('://meta.codereview')
 		? 'meta'
-		: 'main')
+		: 'main') + '_skin')
 );
 
 GM_addStyle(GM_getResourceText(theme));
