f53f5255910740e108ebc859ff572c4eef2f28e6
The-Quill <baileysheather.98@gmail.com>
2015-09-14 14:07:21 +1000

    added meta options

--- design.user.js
+++ design.user.js
@@ -1,16 +1,22 @@
 // ==UserScript==
-// @name         CodeReview dark theme
-// @namespace    https://github.com/The-Quill/Code-Review-Design
-// @version      0.1
-// @description  A new Code Review theme.
-// @author       Quill
-// @match        *//codereview.stackexchange.com
-// @match		 *//codereview.stackexchange.com/*
-// @match        *//meta.codereview.stackexchange.com
-// @match		 *//meta.codereview.stackexchange.com/*
-// @grant		 GM_addStyle
-// @grant        GM_getResourceText
-// @resource dark_theme https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/design.css
+// @name	CodeReview dark theme
+// @version	0.5
+// @description	A new Code Review theme.
+// @author	Quill
+// @match	*://codereview.stackexchange.com
+// @match	*://codereview.stackexchange.com/*
+// @match	*://meta.codereview.stackexchange.com
+// @match	*://meta.codereview.stackexchange.com/*
+// @grant	GM_addStyle
+// @grant	GM_getResourceText
+// @resource	dark_theme https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/design.css
+// @resource	meta_dark_theme https://raw.githubusercontent.com/The-Quill/Code-Review-Design/master/meta_design.css
 // ==/UserScript==
 
-GM_addStyle(GM_getResourceText("dark_theme"));
+var style = (
+	window.location.href.indexOf('meta.codereview.stackexchange.com') !== -1 
+		? 'meta_'
+		: '')
+	+ 'dark_theme';
+
+GM_addStyle(GM_getResourceText(style));
